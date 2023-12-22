af3ae0a27b9868ffbd00204e85a74a0b3f6d142c
itc-s23016 <s23016@std.it-college.ac.jp>
2023-12-22 01:37:00 +0000

    lesson 6.4
    サイトに関する情報を共有できる形で用意する

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,11 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 const Blog = () => {
   const props = { title: 'Blog', subtitle: 'Recent Posts' }
   return (
     <Container>
+      <Meta pageTitle='ブログ' />
       <Hero {...props} />
     </Container>
   )
