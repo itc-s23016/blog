62badf21289dc432109d709bea4f41f7f17b755a
itc-s23016 <s23016@std.it-college.ac.jp>
2024-01-18 07:17:20 +0000

    replace

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    domains: ['images.microcms-assets.io']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
+        port: ''
+      }
+    ]
   }
 }
-
 module.exports = nextConfig
